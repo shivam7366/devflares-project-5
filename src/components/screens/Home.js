e32131597679e14c81091e6258c3c4e66961254b
Shivam Gupta <shivamkhandelwal1623@gmail.com>
2022-12-23 10:32:47 +0530

    third commit

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Home() {
-  return <div>This is home page</div>;
+  return (
+    <div>
+      <>
+        <br />
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">Hackathon</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    </div>
+  );
 }
 
 export default Home;
